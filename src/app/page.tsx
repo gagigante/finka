87d935a2a73dd35d7feaa68dd76fa353285e3527
Gabriel Gigante <gabrielgigante29@gmail.com>
2025-08-13 23:20:27 -0300

    fix: fixes logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-import { useStoreUserEffect } from "@/hooks/use-store-user-effect";
+import { Authenticated } from "convex/react";
+
 import { useTasks } from "@/hooks/queries/tasks";
+import { useStoreUserEffect } from "@/hooks/use-store-user-effect";
 
 import { AppHeader } from "@/components/app-header";
 import { TaskTable } from "@/components/task-table";
 import { DataTable } from "@/components/data-table";
 
-export default function Home() {
+function Content() {
   useStoreUserEffect()
 
   const { data } = useTasks()
@@ -15,7 +17,7 @@ export default function Home() {
   return (
     <div className="flex w-full max-w-[1366px] h-full mx-auto">
       <div className="flex flex-1 flex-col space-y-8 p-8">
-        <AppHeader title="Tarefas" description="Gerencie as tarefas do sistema"/>
+        <AppHeader title="Tarefas" description="Gerencie as tarefas do sistema" />
 
         <TaskTable.Root data={data ?? []}>
           <TaskTable.Toolbar />
@@ -23,5 +25,13 @@ export default function Home() {
         </TaskTable.Root>
       </div>
     </div>
+  )
+}
+
+export default function Home() {
+  return (
+    <Authenticated>
+      <Content />
+    </Authenticated>
   );
 }
